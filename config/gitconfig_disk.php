e7a17d208b6e37256b4a7e22b2cc5ba5fd9012bd
aarontong <284565296@qq.com>
2019-10-01 21:29:14 +0800

    自动部署

---
+++ config/gitconfig_disk.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/10/1
+ * Time: 16:50
+ */
+$config = [
+    [
+        'url' => '',
+        'path' => '',
+        'cmd' => [
+            'pull_master' => 'git pull origin master',
+            'pull' => 'git pull',
+        ]
+    ]
+];
